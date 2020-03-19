facf8952ca17e1a2078aaddc395de8c482913189
Pondorasti <alexandru_turcanu@ymail.com>
2020-03-19 14:43:38 +0200

    Initial Commit

---
+++ BreathingAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BreathingAnimation
+//
+//  Created by Alexandru Turcanu on 19/03/2020.
+//  Copyright © 2020 CodingBytes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
